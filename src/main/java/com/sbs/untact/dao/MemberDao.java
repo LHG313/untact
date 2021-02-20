dade15c26c9271e09f4e50e054a8e7e879d99a33
LHG313 <dd31391@gmail.com>
2021-02-20 15:23:10 +0900

    회원정보 수정

--- src/main/java/com/sbs/untact/dao/MemberDao.java
+++ src/main/java/com/sbs/untact/dao/MemberDao.java
@@ -12,6 +12,8 @@ public interface MemberDao {
 	public void join(Map<String, Object> param);
 
 	public Member getMember(@Param("id") int id);
-	
+
 	public Member getMemberByLoginId(@Param("loginId") String loginId);
+
+	public void modifyMember(Map<String, Object> param);
 }
